1bbc696f287b74feee2b2ce533612151e59846c7
Eric Wong <wsdjeg@outlook.com>
2025-10-06 00:26:00 +0800

    fix setup function

--- lua/picker/init.lua
+++ lua/picker/init.lua
@@ -1,11 +1,15 @@
 local M = {}
 
-local util = require('picker.util')
+local util = require("picker.util")
 
 function M.open(argv)
-    util.info('argv is:' .. vim.inspect(argv))
-    local source = require('picker.sources.' .. argv[1] )
-    require('picker.windows').open(source)
+	util.info("argv is:" .. vim.inspect(argv))
+	local source = require("picker.sources." .. argv[1])
+	require("picker.windows").open(source)
+end
+
+function M.setup(opt)
+	require("picker.config").setup(opt)
 end
 
 return M

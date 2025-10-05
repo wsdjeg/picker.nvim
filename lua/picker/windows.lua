f2fce2938c20d8d6215179465298a251900da291
Eric Wong <wsdjeg@outlook.com>
2025-10-05 22:07:51 +0800

    update windows module

--- lua/picker/windows.lua
+++ lua/picker/windows.lua
@@ -2,10 +2,10 @@ local M = {}
 
 --- what the fuck?
 --- 没办法禁用 cmp，只能 require 后 setup？？
-local ok, cmp = pcall(require, 'cmp')
+local ok, cmp = pcall(require, "cmp")
 if not ok then
-    vim.cmd('doautocmd InsertEnter')
-    ok, cmp = pcall(require, 'cmp')
+	vim.cmd("doautocmd InsertEnter")
+	ok, cmp = pcall(require, "cmp")
 end
 
 local list_winid = -1
@@ -33,7 +33,7 @@ function M.open(source)
 	-- 整体高度：lines 的 80%
 	local screen_height = math.floor(vim.o.lines * config.window.height)
 	if not vim.api.nvim_buf_is_valid(list_bufnr) then
-		list_bufnr = vim.api.nvim_create_buf(false, true)
+		list_bufnr = vim.api.nvim_create_buf(false, false)
 	end
 	if not vim.api.nvim_win_is_valid(list_winid) then
 		list_winid = vim.api.nvim_open_win(list_bufnr, false, {
@@ -51,7 +51,7 @@ function M.open(source)
 		})
 	end
 	if not vim.api.nvim_buf_is_valid(promot_bufnr) then
-		promot_bufnr = vim.api.nvim_create_buf(false, true)
+		promot_bufnr = vim.api.nvim_create_buf(false, false)
 	end
 	if not vim.api.nvim_win_is_valid(promot_winid) then
 		promot_winid = vim.api.nvim_open_win(promot_bufnr, true, {
@@ -76,37 +76,45 @@ function M.open(source)
 			local input = vim.api.nvim_buf_get_lines(promot_bufnr, 0, 1, false)[1]
 			if input ~= "" then
 				local result = source.get()
-                local fzy = require('picker.matchers.fzy')
+				local fzy = require("picker.matchers.fzy")
 				table.sort(result, function(a, b)
 					return fzy.score(input, a) > fzy.score(input, b)
 				end)
 
-				vim.api.nvim_buf_set_lines(list_bufnr, 0, -1, false, vim.tbl_filter(function(t)
-					return fzy.score(input, t) ~= -math.huge
-				end, result))
+				vim.api.nvim_buf_set_lines(
+					list_bufnr,
+					0,
+					-1,
+					false,
+					vim.tbl_filter(function(t)
+						return fzy.score(input, t) ~= -math.huge
+					end, result)
+				)
 			else
 				vim.api.nvim_buf_set_lines(list_bufnr, 0, -1, false, source.get())
 			end
 		end,
 	})
 	vim.keymap.set("i", "<Esc>", function()
+		vim.cmd("noautocmd stopinsert")
 		vim.api.nvim_win_close(promot_winid, true)
 		vim.api.nvim_win_close(list_winid, true)
 	end, { buffer = promot_bufnr })
 	vim.keymap.set("i", "<Enter>", function()
+		vim.cmd("noautocmd stopinsert")
 		local cursor = vim.api.nvim_win_get_cursor(list_winid)
-		local selected = vim.api.nvim_buf_get_lines(list_bufnr, cursor[1], cursor[1], false)[1]
+		local selected = vim.api.nvim_buf_get_lines(list_bufnr, cursor[1] - 1, cursor[1], false)[1]
 		vim.api.nvim_win_close(promot_winid, true)
 		vim.api.nvim_win_close(list_winid, true)
 		source.default_action(selected)
 	end, { buffer = promot_bufnr })
-    if ok then
-        cmp.setup.buffer({
-            completion = {
-                autocomplete = false,
-            },
-        })
-    end
+	if ok then
+		cmp.setup.buffer({
+			completion = {
+				autocomplete = false,
+			},
+		})
+	end
 	vim.cmd("startinsert | doautocmd TextChangedI")
 end
 

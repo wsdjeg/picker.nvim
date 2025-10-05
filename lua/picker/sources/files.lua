4b7915349e8dde7bcc89bbc6a0be72524a7534f4
Eric Wong <wsdjeg@outlook.com>
2025-10-05 22:22:34 +0800

    fix files source

--- lua/picker/sources/files.lua
+++ lua/picker/sources/files.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 function M.get()
+
+    return vim.split(vim.system({'rg', '--files'}, {text = true}):wait().stdout, "\n", {trimempty = true})
     
+end
+
+function M.default_action(s)
+    vim.cmd('edit ' .. s)
 end
 
 

574b2b6713b1f9ab2e732e58b8d49c7c1ea500d9
Eric Wong <wsdjeg@outlook.com>
2025-10-05 23:11:49 +0800

    complete sources

--- plugin/picker.lua
+++ plugin/picker.lua
@@ -1,5 +1,12 @@
-vim.api.nvim_create_user_command('Picker', function(opt)
-
-    require('picker').open(opt.fargs)
-
-end, {nargs = '*'})
+vim.api.nvim_create_user_command("Picker", function(opt)
+	require("picker").open(opt.fargs)
+end, {
+	nargs = "*",
+	complete = function(ArgLead, CmdLine, CursorPos)
+		local sources = vim.api.nvim_get_runtime_file("lua/picker/sources/*.lua", true)
+		local rst = vim.tbl_map(function(t)
+			return vim.fn.fnamemodify(t, ":t:r")
+		end, sources)
+        return vim.tbl_filter(function(t) return vim.startswith(t, ArgLead) end, rst)
+	end,
+})

4915327d84085ca32f28d664643672dba03cbde0
Eric Wong <wsdjeg@outlook.com>
2025-10-08 17:54:04 +0800

    fix: handle unknown source

--- lua/picker/init.lua
+++ lua/picker/init.lua
@@ -4,8 +4,12 @@ local util = require("picker.util")
 
 function M.open(argv)
 	util.info("argv is:" .. vim.inspect(argv))
-	local source = require("picker.sources." .. argv[1])
-	require("picker.windows").open(source)
+	local ok, source = pcall(require, "picker.sources." .. argv[1])
+	if not ok then
+        util.notify(string.format('can not found source "%s" for picker.nvim', argv[1]))
+	else
+		require("picker.windows").open(source)
+	end
 end
 
 function M.setup(opt)

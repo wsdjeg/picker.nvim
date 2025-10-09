15c8322f361eaa57341cc444f84e50dea8b1add4
Eric Wong <wsdjeg@outlook.com>
2025-10-09 21:18:17 +0800

    enable preview for mru source

--- lua/picker/sources/mru.lua
+++ lua/picker/sources/mru.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+local previewer = require('picker.previewer.file')
+
+
+M.preview_win = true
+
+function M.preview(item, win, buf)
+    previewer.preview(item, win, buf)
+end
+
 function M.get()
 
     return require('mru').get()

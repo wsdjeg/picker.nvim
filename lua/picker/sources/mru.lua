91cce3e9703ab5fe9417c8f6b4003da768e60dc6
Eric Wong <wsdjeg@outlook.com>
2025-10-10 11:47:15 +0800

    update

--- lua/picker/sources/mru.lua
+++ lua/picker/sources/mru.lua
@@ -1,24 +1,21 @@
 local M = {}
 
-local previewer = require('picker.previewer.file')
-
+local previewer = require("picker.previewer.file")
 
 M.preview_win = true
 
 function M.preview(item, win, buf)
-    previewer.preview(item, win, buf)
+	previewer.preview(item, win, buf)
 end
 
 function M.get()
-
-    return require('mru').get()
-    
+	return vim.tbl_map(function(t)
+		return { value = t, str = t }
+	end, require("mru").get())
 end
 
 function M.default_action(s)
-    vim.cmd('edit ' .. s)
+	vim.cmd("edit " .. s)
 end
 
-
 return M
-

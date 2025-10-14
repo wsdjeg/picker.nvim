83599fd5b143d53b3bb48dd578550f3df0f4e2e0
Eric Wong <wsdjeg@outlook.com>
2025-10-14 21:47:28 +0800

    feat: enable preview for buffers source

--- lua/picker/sources/buffers.lua
+++ lua/picker/sources/buffers.lua
@@ -1,23 +1,32 @@
 local M = {}
 
+local previewer = require("picker.previewer.file")
+
 function M.get()
 	local bufnrs = vim.tbl_filter(function(bufnr)
 		if 1 ~= vim.fn.buflisted(bufnr) then
 			return false
 		end
-        return true
+		return true
 	end, vim.api.nvim_list_bufs())
 
 	return vim.tbl_map(function(t)
-        return {
-            value = t,
-            str = vim.api.nvim_buf_get_name(t)
-        }
+		return {
+			value = t,
+			str = vim.api.nvim_buf_get_name(t),
+		}
 	end, bufnrs)
 end
 
 function M.default_action(s)
 	vim.cmd("edit " .. s.str)
+end
+
+M.preview_win = true
+
+---@field item PickerItem
+function M.preview(item, win, buf)
+	previewer.preview(item.str, win, buf)
 end
 
 return M

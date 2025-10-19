de9b6833dd3ab33253fcb80ccfd12c3c4f7ca27c
Eric Wong <wsdjeg@outlook.com>
2025-10-19 15:55:02 +0800

    fix buf context

--- lua/picker/sources/jumps.lua
+++ lua/picker/sources/jumps.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local previewer = require("picker.previewer.file")
+local previewer = require("picker.previewer.buffer")
 
 function M.get()
 	return vim.tbl_map(function(t)
@@ -16,6 +16,16 @@ end
 function M.default_action(item)
 	vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), item.value.bufnr)
 	vim.api.nvim_win_set_cursor(0, { item.value.lnum, item.value.col })
+end
+
+M.preview_win = true
+
+---@field item PickerItem
+function M.preview(item, win, buf)
+	previewer.buflines = vim.api.nvim_buf_get_lines(item.value.bufnr, 0, -1, false)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, previewer.buflines)
+	previewer.filetype = vim.api.nvim_get_option_value("filetype", { buf = item.value.bufnr })
+	previewer.preview(item.value.lnum, win, buf)
 end
 
 return M

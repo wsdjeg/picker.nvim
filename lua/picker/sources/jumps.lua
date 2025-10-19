7fe98e3e7dde817b20504f0ab5d0b67cc3ae05a5
Eric Wong <wsdjeg@outlook.com>
2025-10-19 16:06:47 +0800

    fix: fix jump source action

--- lua/picker/sources/jumps.lua
+++ lua/picker/sources/jumps.lua
@@ -1,10 +1,14 @@
 local M = {}
 
-local previewer = require("picker.previewer.buffer")
+local previewer = require("picker.previewer.file")
 
 function M.get()
 	return vim.tbl_map(function(t)
-		local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(t.bufnr), ":.")
+		local filename = vim.api.nvim_buf_get_name(t.bufnr)
+        t.filename = t.filename or filename
+        if #filename == 0 then
+            filename = 'No Name'
+        end
 		return {
 			value = t,
 			str = string.format("%s:%d:%d", filename, t.lnum, t.col),
@@ -14,18 +18,15 @@ end
 
 ---@field item PickerItem
 function M.default_action(item)
-	vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), item.value.bufnr)
-	vim.api.nvim_win_set_cursor(0, { item.value.lnum, item.value.col })
+    vim.api.nvim_win_set_buf(0, item.value.bufnr)
+    vim.api.nvim_win_set_cursor(0, {item.value.lnum, item.value.col})
 end
 
 M.preview_win = true
 
 ---@field item PickerItem
 function M.preview(item, win, buf)
-	previewer.buflines = vim.api.nvim_buf_get_lines(item.value.bufnr, 0, -1, false)
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, previewer.buflines)
-	previewer.filetype = vim.api.nvim_get_option_value("filetype", { buf = item.value.bufnr })
-	previewer.preview(item.value.lnum, win, buf)
+    previewer.preview(item.value.filename, win, buf, item.value.lnum)
 end
 
 return M

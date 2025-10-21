c287aa165cb668a076e9d463bf851a12f152cbb6
Eric Wong <wsdjeg@outlook.com>
2025-10-21 22:03:01 +0800

    feat: async support for source

---
+++ lua/picker/sources/async_files.lua
@@ -0,0 +1,71 @@
+local M = {}
+
+local previewer = require("picker.previewer.file")
+
+local job = require("job")
+
+---@type PickerItem[]
+local items = {}
+
+local cmd = { "rg", "--files" }
+
+
+local jobid = -1
+
+local function on_stdout(id, data)
+	if id == jobid then
+		vim.tbl_map(function(t)
+			table.insert(items, {
+				value = t,
+				str = t,
+			})
+		end, data)
+		vim.cmd("doautocmd TextChangedI")
+	end
+end
+local function on_stderr(id, data) end
+local function on_exit(id, data, singin)
+	if id == jobid then
+		jobid = -1
+	end
+		vim.cmd("doautocmd TextChangedI")
+end
+
+local function async_run()
+	if jobid > 0 then
+		job.stop(jobid)
+	end
+	jobid = job.start(cmd, {
+		on_stdout = on_stdout,
+		on_stderr = on_stderr,
+		on_exit = on_exit,
+	})
+	return items
+end
+function M.set(opt)
+	opt = opt or {}
+
+	cmd = opt.cmd or cmd
+	items = {}
+
+	async_run()
+
+end
+
+function M.get()
+	return items
+end
+
+---@field item PickerItem
+function M.default_action(item)
+	vim.cmd("edit " .. item.value)
+end
+
+M.preview_win = true
+
+---@field item PickerItem
+function M.preview(item, win, buf)
+	previewer.preview(item.value, win, buf)
+end
+
+return M

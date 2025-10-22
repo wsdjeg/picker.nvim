7b0a2bf529b55b82fec19327349f3d585427a704
Eric Wong <wsdjeg@outlook.com>
2025-10-22 23:41:44 +0800

    update

--- lua/picker/filter.lua
+++ lua/picker/filter.lua
@@ -2,17 +2,12 @@ local M = {}
 
 local fzy = require("picker.matchers.fzy")
 
-local ut = require('picker.util')
-
 local tid = -1
 
 function M.filter(input, source, callback)
-    ut.info(input)
-    ut.info(vim.inspect(source))
 	vim.fn.timer_stop(tid)
 	local function async_task()
-		-- 每次取 5000 个 进行匹配排序
-		local count = 100
+		local count = 5000
 		if #input == 0 then
 			source.filter_items = {}
 			source.state.filter_count = 0
@@ -55,7 +50,6 @@ function M.filter(input, source, callback)
 					end
 				end
 			else
-				source.state.saved_filter_items = source.filter_items
 				source.filter_items = {}
                 source.state.filter_count = 0
 				for i = 1, #source.state.items do
@@ -75,7 +69,8 @@ function M.filter(input, source, callback)
 	local co = coroutine.create(async_task)
 
 	local function loop(t)
-		if source.state.filter_count == #source.state.items then
+        --- dead，suspended，running
+		if coroutine.status(co) == 'dead' then
 			vim.fn.timer_stop(tid)
 			return
 		end
